2cddd620051ed4dda628f6a4c43738a981010467
calidiHawk <bross@calidicreative.com>
2018-01-28 18:21:31 -0500

    Added Client Side Modal Window

--- vue-hot-dev/vueDev/src/App.vue
+++ vue-hot-dev/vueDev/src/App.vue
@@ -2,10 +2,15 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+
+
+
   </div>
 </template>
 
 <script>
+
+
 export default {
   name: 'App'
 }
--- vue-hot-dev/vueDev/src/components/HelloWorld.vue
+++ vue-hot-dev/vueDev/src/components/HelloWorld.vue
@@ -1,5 +1,19 @@
 <template>
+
   <div class="hello">
+
+    <demo-dog-profile-modal />
+
+    <button
+      class="btn green"
+      @click="$modal.show('dog-profile')">
+      Demo: Dog Profile photo
+    </button>
+
+    <modal name="hello-world">
+      hello, world
+    </modal>
+
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
@@ -84,14 +98,93 @@
 </template>
 
 <script>
+import DemoDogProfileModal  from './DogProfileModal.vue'
+
 export default {
   name: 'HelloWorld',
-  data () {
+  components: {
+    DemoDogProfileModal
+  },
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      resizable: false,
+      adaptive: false,
+      draggable: false,
+      canBeShown: false
     }
-  }
+  },
+  created () {
+    setInterval(() => {
+      this.canBeShown = !this.canBeShown
+    }, 5000)
+  },
+  methods: {
+    show(resizable, adaptive, draggable) {
+      this.resizable = resizable
+      this.adaptive = adaptive
+      this.draggable = draggable
+      /*
+        $nextTick is required because the data model with new
+        "resizable, adaptive, draggable" values is not updated yet.. eh
+      */
+      this.$nextTick(() => {
+        this.$modal.show('example-modal')
+      })
+    },
+
+    conditionalShow () {
+      this.$modal.show('conditional-modal', { show: this.canBeShown })
+    },
+
+
+    showBasicDialog () {
+      this.$modal.show('dialog', {
+        text: 'I am a tiny dialog box.<br/>And I render <b>HTML!</b>'
+      })
+    },
+
+    showTitleDialog () {
+      this.$modal.show('dialog', {
+        title: 'Information',
+        text: 'Check out, I have a title 😎'
+      })
+    },
+
+    showButtonsDialog () {
+      this.$modal.show('dialog', {
+        title: 'Buttons example',
+        text: 'You can add an arbitrary number of buttons.',
+        buttons: [
+          {
+            title: 'C💩NCEL',
+            handler: () => {
+              this.$modal.hide('dialog')
+            }
+          },
+          {
+            title: 'LIKE',
+            default: true,
+            handler: () => {
+              alert('LIKE LIKE LIKE')
+            }
+          },
+          {
+            title: 'REPOST',
+            handler: () => {
+              alert('REPOST REPOST REPOST')
+            }
+          }
+        ]
+      })
+    },
+
+    dialogEvent (eventName) {
+      console.log('Dialog event: ' + eventName)
+    }
+  },
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
